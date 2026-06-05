56996143b125d39a9b816bcacc00fb716d26a88c
yushan wang <yushan.wang@cea.fr>
2026-06-05 15:29:09 +0200

    clean up

--- apps/compression/gys_compress.cpp
+++ apps/compression/gys_compress.cpp
@@ -430,8 +430,6 @@ int main(int argc, char **argv) {
   }
 
   steady_clock::time_point const start = steady_clock::now();
-  
-  ddc::PdiEvent("initial_catalyst");
   predcorr(get_field(allfdistribu_v2D_split), deltat, nbiter);
 
   steady_clock::time_point const end = steady_clock::now();
--- apps/compression/python_prep3.py
+++ apps/compression/python_prep3.py
@@ -6,10 +6,7 @@ import numpy as np
 def print_mesh_info(meshx_py, meshy_py, meshvx_py, meshvy_py, local_fdistribu_extents_py, local_fdistribu_starts_py) :
     # construir le mesh 
     print("========================calling print_mesh_info")
-    
     print("\n===================meshvx_py = ", meshvx_py)
-
-    
 
 def prep4catalyst(fdistribu_py, electrostatic_potential_py, iter_py, time_py, meshx_py, meshy_py, meshvx_py, meshvy_py, local_fdistribu_extents_py, local_fdistribu_starts_py):
     comm = MPI.COMM_WORLD
@@ -18,10 +15,6 @@ def prep4catalyst(fdistribu_py, electrostatic_potential_py, iter_py, time_py, me
     if rank ==0: 
         print("========================calling prep4catalyst at iteration ", iter_py)
 
-    # print("\n=================== rank ", rank, ": local_fdistribu_extents_py = ", local_fdistribu_extents_py)
-
-    # print("\n=================== rank ", rank, ": local_fdistribu_starts_py = ", local_fdistribu_starts_py)
-
     PT_Y=int(local_fdistribu_extents_py[2]/2)
     PT_VY=int(local_fdistribu_extents_py[4]/2)
 
@@ -29,11 +22,6 @@ def prep4catalyst(fdistribu_py, electrostatic_potential_py, iter_py, time_py, me
     
     coord_X = meshvx_py[local_fdistribu_starts_py[3]:local_fdistribu_starts_py[3]+local_fdistribu_extents_py[3]]
 
-    # print("\n=================== rank ", rank, ": coord_X = ", coord_X)
-    # coordonnee dans le champs de vitesse
-
-    # print("\n=================== rank ", rank, ": selected = ", fdistribu_selected)
-        # print("rank ", rank, ": Coord_X = ", coord_X)
     if iter_py%10 == 0:
         pdi.multi_expose("catalyst_execute", [
                     ("cycle", iter_py, pdi.OUT),

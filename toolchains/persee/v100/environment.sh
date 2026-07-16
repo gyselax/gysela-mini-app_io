3eb9c8a9ffa232935b6d02906c0296b583a33552
DeVora <ariel.devora@cea.fr>
2026-07-16 11:45:35 +0200

    Add persee xeon and v100 environment.sh, add toolchains.cmake

--- toolchains/persee/v100/environment.sh
+++ toolchains/persee/v100/environment.sh
@@ -19,7 +19,7 @@ fi
 # The hdf5 package is injecting the environment view `lib` path to `LD_LIBRARY_PATH`
 # which causes spurious segfaults for system executables, we manually remove it.
 LD_LIBRARY_PATH_TMP="$LD_LIBRARY_PATH"
-spack env activate /data/gyselarunner/gysela-io-env-deisa/
+spack env activate /data/gyselarunner/gysela-io-env-deisa-cuda/
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH_TMP"
 unset LD_LIBRARY_PATH_TMP
 
@@ -29,7 +29,7 @@ unset LD_PRELOAD
 
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=8
 
 # Add Kokkos Tools to the `LD_LIBRARY_PATH`
 export LD_LIBRARY_PATH="$(spack location -i kokkos-tools)/lib64:$LD_LIBRARY_PATH"
